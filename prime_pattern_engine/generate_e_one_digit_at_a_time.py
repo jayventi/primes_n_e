879c3173e211876c3d5df03c49a5cbf71c2914da
Jay Venti <jayventi@gmail.com>
2016-09-29 16:35:57 -0700

    Continued fraction annotation and clean

--- prime_pattern_engine/generate_e_one_digit_at_a_time.py
+++ prime_pattern_engine/generate_e_one_digit_at_a_time.py
@@ -3,37 +3,34 @@
 # found at: http://stackoverflow.com/questions/9144154/code-to-generate-e-one-digit-at-a-time
 # see: Generating digits of square root of 2
 # at: http://stackoverflow.com/questions/5187664/generating-digits-of-square-root-of-2
-
+#
+# brief description of continuous fraction method
+# any fraction may be given as a continuous fraction representation finite continuous fraction elements
+# irrational numbers such as e can be given with infinite sequences of continuous fraction terms
+# e is given by a repetitive pattern of continuous fraction terms [1;0,1,1,2,1,1,4...] 
+# for a tutorial on continuous fractions see 
 
 def z(contfrac, a=1, b=0, c=0, d=1):
     for x in contfrac:
+        # x will be the  continued fraction as input
         while a > 0 and b > 0 and c > 0 and d > 0:
             t = a // c
             t2 = b // d
             if not t == t2:
                 break
+            #print t
             yield t
             a = (10 * (a - c*t))
             b = (10 * (b - d*t))
             # continue with same fraction, don't pull new x
         a, b = x*a+b, a
         c, d = x*c+d, c
-    for digit in rdigits(a, c):
-        yield digit
-
-
-def rdigits(p, q):
-    while p > 0:
-        if p > q:
-            d = p // q
-            p = p - q * d
-        else:
-            d = (10 * p) // q
-            p = 10 * p - q * d
-        yield d
+    
 
 
 def e_cf_expansion():
+    #  continued fraction for e []
+    #  see Representations in en.wikipedia.org/wiki/E_(mathematical_constant)
     yield 1
     k = 0
     while True:
@@ -49,13 +46,13 @@ def e_dec():
 
 if __name__ == '__main__':
     # configuration parameters
-    digit_count = 300000
+    digit_count = 1200
     work_dir = 'work_files/'
 
     gen = e_dec()
     e = [str(gen.next()) for i in xrange(digit_count)]
 
     e_str = ''.join(e)
-    #print e_str
-    with open("{0}e_digit_{1}.txt".format(work_dir, digit_count), "w") as text_file:
-        text_file.write(e_str)
+    print e_str
+    # with open("{0}e_digit_{1}.txt".format(work_dir, digit_count), "w") as text_file:
+    #     text_file.write(e_str)
